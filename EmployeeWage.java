e044fa7d597f79fbdccc2d749caf1d34c23bf473
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-11-24 23:03:11 +0530

    Added UC:1

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,9 +6,28 @@ public class EmployeeWage {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage  Computation Program");
 
+		checkEmpPresentOrAbsent();
+
 	}
 
+	/*
+	 * @ params : None
+	 * 
+	 * @ return : None
+	 * 
+	 * @Description : UC-1 Check Employee is Present or Absent
+	 */
 
+	public static int checkEmpPresentOrAbsent() {
+		int PRESENT = 1;
+		int empCheck = RandomGenerator.generateattendance();
 
+		if (empCheck == PRESENT)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+
+		return empCheck;
+	}
 
 }
