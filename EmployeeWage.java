b4cea27907416ae52c96396cd9a1d4d93f6ea356
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-11-24 23:06:24 +0530

    Added UC: 2

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,14 @@
 package com.bridgelabs.Master;
 
+import com.bridgelabs.usecase1.Attendance;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage  Computation Program");
 
-		checkEmpPresentOrAbsent();
+		calculateDailyEmployeeWage();
 
 	}
 
@@ -28,6 +30,33 @@ public class EmployeeWage {
 			System.out.println("Employee is Absent");
 
 		return empCheck;
+	}
+
+	/*
+	 * @params : None
+	 * 
+	 * @return : int
+	 * 
+	 * @Description : UC-2 Calculate Daily Employee Wage
+	 */
+
+	public static int calculateDailyEmployeeWage() {
+
+		int attend = Attendance.checkEmpPresentOrAbsent();
+		int full_time = 1;
+		int wage_per_hour = 20;
+		int empHrs = 0;
+		int empWage = 0;
+
+		if (attend == full_time)
+			empHrs = 8;
+		else
+			empHrs = 0;
+
+		empWage = empHrs * wage_per_hour;
+		System.out.println("Employee Wage : " + empWage);
+		return empWage;
+
 	}
 
 }
